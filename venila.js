9dc791fb05cb02e5957354b3f69fb99fbad9c0da
sridharSTR <127420091+sridharSTR@users.noreply.github.com>
2024-01-23 17:11:24 +0530

    v-1.00.01

--- venila.js
+++ venila.js
@@ -4,33 +4,42 @@ const openers = document.getElementById('opener')
 const rate = document.getElementById('rate')
 vars.innerHTML = num
 let x = 125.00
-rate.innerHTML = x
+//rate.innerHTML = x
 function Caly(e){
   let dummy = 125.00
-if(e == '+'){
-  num++
-  x = x+dummy
-  rate.innerHTML = x
-  vars.innerHTML = num
+if(e == '-' && num > 0){
+  num--
+x = x-dummy
+//rate.innerHTML = x
+vars.innerHTML= num
+
 
 }
 
 else{
-num--
-x = x-dummy
-rate.innerHTML = x
-vars.innerHTML= num
+
+num++
+  x = x+dummy
+  //rate.innerHTML = x
+  vars.innerHTML = num
 }
 }
 
+const contain = document.getElementById('contain')
 function Onloader(){
-  if(openers.style.display == 'none'){
-    openers.style.display = 'flex'
+  if(openers.style.display == 'flex'){
+    openers.style.display = 'none'
   }
   else{
+    openers.style.display = 'flex'
+  }
+}
+function mYfun(){
+  if(openers.style.display = 'flex'){
     openers.style.display = 'none'
   }
 }
+contain.addEventListener('click',mYfun)
 
 // let xyx = {
 //   img0 : './images/icon-cart.svg',
@@ -49,6 +58,7 @@ let ary = [
 const newimg = document.getElementById('newimg')
 let num23 = 0
 newimg.setAttribute('src',ary[num23])
+newimg.style.borderRadius = '2vmin'
 
 let n23 = 0
   function Moverfor(s){
@@ -79,3 +89,83 @@ let n23 = 0
 
     }
   }
+
+  // let xyx67 = {
+  //   //   img0 : './images/icon-cart.svg',
+  //      img1 : "./images/image-product-1-thumbnail.jpg",
+  //      img2 : "./images/image-product-2-thumbnail.jpg",
+  //      img3 : "./images/image-product-3-thumbnail.jpg",
+  //      img4 : "./images/image-product-4-thumbnail.jpg"
+    
+  //    }
+function thumbnailFunc(n){
+    console.log(n);
+    switch (n) {
+      case 1:
+        newimg.setAttribute('src',ary[0])
+        break;
+      case 2:
+        newimg.setAttribute('src',ary[1])
+        break;
+      case 3:
+        newimg.setAttribute('src',ary[2])
+        break;
+      case 4:
+        newimg.setAttribute('src',ary[3])
+        break;
+      default:
+        break;
+    }
+}
+const gallery = document.getElementById('gallery')
+const mainImg = document.getElementById('main-img')
+
+
+function productView(event){
+  var pageWidth = document.documentElement.scrollWidth;
+if(pageWidth > 780){
+  let newURL = event.srcElement.src
+  //console.log(newURL);
+  gallery.style.display = 'block'
+  mainImg.setAttribute('src',newURL)
+}
+  
+}
+
+
+function closeFunc(){
+  gallery.style.display = 'none'
+}
+const price = document.getElementById('prices')
+const number = document.getElementById('number')
+const emptyC = document.getElementById('emptyC')
+const carticon = document.getElementById('cartIcon')
+function cartLoader(){
+  Load.style.display = 'grid'
+  emptyC.style.display = 'none'
+  let size = vars.innerText
+  number.innerText = size
+  price.innerText = size*125.00
+  carticon.style.borderBottom = '5px solid orangered'
+  
+}
+const Load = document.getElementById('loader')
+function clearCart(){
+  emptyC.style.display = 'block'
+  Load.style.display = 'none'
+  carticon.style.borderBottom = ''
+
+}
+
+const menubox = document.getElementById('menuBox')
+
+function openMenu(opp){
+  if(opp){
+    menubox.style.display = 'flex'
+  }
+  else{
+    menubox.style.display = 'none'
+  }
+}
+
+
